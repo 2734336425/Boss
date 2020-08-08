6ad06069978558da1dae7378e90d6f55e440670e
2734336425 <48973051+2734336425@users.noreply.github.com>
2020-08-08 10:59:34 +0800

    Update boss.js

--- js/boss.js
+++ js/boss.js
@@ -10,3 +10,18 @@ $(".prev").on("click",function(){
 	$(".next").show();
 	$(".slider-city ul").css("transform","translateX(0px)");
 })
+$(".up").click(function(){
+	$(this).toggleClass(function(){
+		if($(this).hasClass("up")){
+		  $(this).removeClass("up");
+		  $(".sss").html("展开");
+		  $(".waibao").css("height","57px");
+		  return "down";
+		}else{
+			$(this).removeClass("down");
+			 $(".sss").html("收起");
+			  $(".waibao").css("height","134px");
+			return "up";
+		}
+	})
+})
